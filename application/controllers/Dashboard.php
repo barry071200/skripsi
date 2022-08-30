88c43540d8c33cdaa88c6dcc0bfc2d71f753a93c
albertus cahaya putra <albertus.cahaya@si.ukdw.ac.id>
2022-08-30 17:44:35 +0700

    commit

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+
+class Dashboard extends CI_Controller
+{
+    
+
+  public function index()
+  {
+    $data['judul'] = "Tabel Contoh";
+    $data['layout'] = "conten";
+    //$this->load->view('templates/header');
+		//$this->load->view('templates/sidebar');
+		$this->load->view('template', $data);
+		//$this->load->view('templates/footer');
+    
+  }
+
+}
+
